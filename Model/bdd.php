fab311124cf5a38cac8b2ec99301404d5663a42f
Laura <laura.rodriguez@laplateforme.io>
2022-01-10 17:22:41 +0100

    maj index

--- Model/bdd.php
+++ Model/bdd.php
@@ -1,5 +1,3 @@
-<?php 
-class Utilisateurs{
-
-}
+<?php
+    $bdd = new PDO('mysql:host=localhost;dbname=reservationsalles;charset=utf8', 'root', '');
 ?>
--- View/HTML/index.php
+++ View/HTML/index.php
@@ -1,11 +1,11 @@
 <?php 
-require('.../Model/bdd.php'); //ne vas pas jusqu'a model reste dans view 
+require('../../Model/bdd.php'); //ne vas pas jusqu'a model reste dans view 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href='CSS/index.css' />
+    <link rel="stylesheet" href='../CSS/index.css' />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Index</title>
 </head>
@@ -16,7 +16,9 @@ require('.../Model/bdd.php'); //ne vas pas jusqu'a model reste dans view
         ?>
     </header>
     <main>
-
+<p>
+    test
+</p>
     </main>
     <footer>
         <?php 
