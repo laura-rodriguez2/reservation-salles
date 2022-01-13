d18de37739106470710a8e907a915f0c72330f67
alex <alex.lovati@laplateforme.io>
2022-01-13 17:00:22 +0100

    index fond image et bloc scroll

--- View/HTML/index.php
+++ View/HTML/index.php
@@ -1,30 +1,36 @@
-<?php
-require('../../Model/bdd.php');
-?>
 <!DOCTYPE html>
 <html>
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href='../CSS/index.css' />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Index</title>
-</head>
-
-<body>
-    <header>
-        <?php
-        require_once('header_footer/header.php');
-        ?>
-    </header>
-    <main>
-        <p>main</p>
-    </main>
-    <footer>
-        <?php
-        require_once('header_footer/footer.php');
-        ?>
-    </footer>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href='../CSS/index.css' />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity=
+        "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"crossorigin="anonymous">
+        <title>Index</title>
+    </head>
+    <body>
+        <header>
+            <?php 
+                require_once("header_footer/header.php")
+            ?>
+        </header>
+        <main>
+            <div class="al_scroll-snap">
+                <div class="al_bloc">
+                    <img src="../MEDIAS/fond1.png">
+                </div>
+                <div class="al_bloc">
+                    <h1>bloc2</h1>
+                </div>
+                <div class="al_bloc">
+                    <h1>bloc3</h1>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <?php 
+                require_once("header_footer/footer.php")
+            ?>
+        </footer>
+    </body>
 
 </html>
-</body>
