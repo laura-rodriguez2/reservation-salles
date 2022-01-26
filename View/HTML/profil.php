800c0b7a5b42c2b8592323e1b002fbfab252f5df
Martinet Laure <laure.martinet@laplateforme.io>
2022-01-26 15:10:32 +0100

    css profil et details profil html

--- View/HTML/profil.php
+++ View/HTML/profil.php
@@ -59,11 +59,11 @@ if (isset($_SESSION['id']) && $_SESSION['id'] > 0) {
     </header>
     <main>
         <form id="form_inscription" action="" method="POST">
-            <h2 class="lr_h2">Modifier mes informations</h1><br>
-                    <input type="text" class="box-input" name="newlogin" value="<?php echo $_SESSION['login']; ?>"/><br>
-                    <input type="password" class="box-input" name="newmdp" placeholder="Mot de passe" required /><br>
-                    <input type="password" class="box-input" name="newmdp2" placeholder="Confirmez votre mot de passe" required /><br><br>
-                    <input type="submit" name="submit" value="Enregistrer mes informations" class="btn btn-secondary btn-lg" /><br><br>
+            <h2 class="lr_h2">Modifier mes informations :</h1><br>
+                    <input type="text" class="box-input" name="newlogin" value="<?php echo $_SESSION['login']; ?>"/>
+                    <input type="password" class="box-input" name="newmdp" placeholder="Mot de passe" required />
+                    <input type="password" class="text" name="newmdp2" placeholder="Confirmez votre mot de passe" required />
+                    <input type="submit" name="submit" value="Enregistrer mes informations" class="btn btn-secondary btn-lg" /><br>
                     <a href="deconnexion.php"><input class="btn btn-secondary btn-lg" type="button" value="Déconnexion"></a>
         </form>
     </main>
