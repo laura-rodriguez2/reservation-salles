38041bcfb7fe676d0f559125b23026346cfe0eff
alex <alex.lovati@laplateforme.io>
2022-01-31 17:18:44 +0100

    CEST BON LE HEADER MARCHE mais cest avec padding '-'

--- View/HTML/header_footer/header.php
+++ View/HTML/header_footer/header.php
@@ -1,47 +1,30 @@
 <?php
 require('../../Model/bdd.php');
 ?>
-
 <!DOCTYPE html>
 <html>
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href='../CSS/header.css' />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body>
-    <header class=al_header>
-        <?php 
-        if(($_SESSION == true)){
+    <head>
+        <meta charset ="utf-8">
+        <link rel="stylesheet" href="../CSS/header.css"  />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+        <?php
+            if(($_SESSION ==true)){
         ?>
-        <nav>  
-            <div class="lr_nav_header">
-                <img src="../MEDIAS/vr_universe.png" width="15%">
-            </div>
-            <ul class="lr_li_header">
-                <a class="al_header_color" href="index.php"> Accueil</a>
-                <a class="al_header_color" href="planning.php"> Planning</a>
-                <a class="al_header_color" href="reservation-form.php"> Réservation</a>
-                <a class="al_header_color" href="profil.php"> Profil </a>
-                <a class="al_header_color" href="deconnexion.php"> Déconnexion</a> 
-            </ul>
-        </nav>
-        <?php 
-            } else{ 
+        <img src="../MEDIAS/vr_universe.png" width="15%">
+        <?php
+            }else{
         ?>
-                <img src="../MEDIAS/vr_universe.png" width="15%">
-        <nav class=position_nav>
-                <a class="al_header_color" href="index.php"> Accueil</a>
-                <a class="al_header_color" href="planning.php"> Planning</a>
-                <a class="al_header_color" href="inscription.php"> Inscription</a>
-                <a class="al_header_color" href="connexion.php"> Connexion</a>
-        </nav>
-        <?php 
+            <img src="../MEDIAS/vr_universe.png" width="15%">
+            <a class="al_header_color" href="index.php">Acceuil</a>
+            <a class="al_header_color" href="planning.php">Planning</a>
+            <a class="al_header_color" href="inscription.php">Inscription</a>
+            <a class="al_header_color" href="connexion.php">Connexion</a>
+        <?php
             }
         ?>
-    </header>
-
+        </header>
+    </body>
 </html>
-</body>
