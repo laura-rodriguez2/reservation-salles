db438a08f2c463ec96701892e1ad902daa653221
Martinet Laure <laure.martinet@laplateforme.io>
2022-02-02 16:11:09 +0100

    add mentions leg

---
+++ View/HTML/mentions.php
@@ -0,0 +1,32 @@
+<?php
+require '../../Model/bdd.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href='../CSS/reservation-form.css' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Mentions légales</title>
+    </head>
+
+    <body>
+        <header>
+            <?php require_once('header_footer/header.php');
+            ?>
+        </header>
+        <main>
+            <h1>Mentions légales</h1>
+            <p>Le présent site internet est édité par la société YMAGIS ENTERTAINMENT SAS au capital de 374332 30000 € enregistré sous le numéro RCS Nanterre 488 397 076, dont le siège social est situé au 40 rue Gabriel Crié, 92240 Malakoff.
+
+Son numéro de téléphone non surtaxé est le 01.75.44.88.76, son adresse de courrier électronique est contact@illucity.fret son numéro individuel d’identification TVA intracommunautaire FR32488397076.
+
+Le présent site internet est hébergé par O.V.H, joignable sur https://www.ovh.com</p>
+        </main>
+        <footer>
+            <?php
+            require_once('header_footer/footer.php');
+            ?>
+        </footer>
+    </body>
+</html>
