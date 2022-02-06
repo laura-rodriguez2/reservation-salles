cc5cb503a7f41cc5c6fb6c4b2a025e793c996436
Laura <laura.rodriguez@laplateforme.io>
2022-02-06 20:51:10 +0100

    modif pages css, indentation
    enlever dans reservation form la date de fin

--- View/HTML/mentions.php
+++ View/HTML/mentions.php
@@ -4,30 +4,33 @@ require '../../Model/bdd.php';
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href='../CSS/planning.css' />
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href='../CSS/planning.css' />
+    <link rel="icon" href="../MEDIAS/vr_universe_icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Mentions légales</title>
-    </head>
+</head>
 
-    <body>
-        <header>
-            <?php require_once('header_footer/header.php');
-            ?>
-        </header>
-        <main>
-            <h1>Mentions légales</h1><br>
-            <p>Le présent site internet est édité par la société YMAGIS ENTERTAINMENT SAS au capital de 374332 30000 € enregistré sous le numéro RCS Nanterre 488 397 076, dont le siège social est situé au 40 rue Gabriel Crié, 92240 Malakoff.
+<body>
+    <header>
+        <?php require_once('header_footer/header.php');
+        ?>
+    </header>
+    <main>
+        <h1>Mentions légales</h1><br>
+        <p>Le présent site internet est édité par la société YMAGIS ENTERTAINMENT SAS au capital de 374332 30000 € enregistré sous le numéro RCS Nanterre 488 397 076, dont le siège social est situé au 40 rue Gabriel Crié, 92240 Malakoff.
 
-Son numéro de téléphone non surtaxé est le 01.75.44.88.76, son adresse de courrier électronique est contact@illucity.fret son numéro individuel d’identification TVA intracommunautaire FR32488397076.
+            Son numéro de téléphone non surtaxé est le 01.75.44.88.76, son adresse de courrier électronique est contact@illucity.fret son numéro individuel d’identification TVA intracommunautaire FR32488397076.
 
-Le présent site internet est hébergé par O.V.H, joignable sur https://www.ovh.com</p>
-        </main>
-        <footer>
-            <?php
-            require_once('header_footer/footer.php');
-            ?>
-        </footer>
-    </body>
+            Le présent site internet est hébergé par O.V.H, joignable sur https://www.ovh.com</p>
+    </main>
+    <footer>
+        <?php
+        require_once('header_footer/footer.php');
+        ?>
+    </footer>
+</body>
+
 </html>
